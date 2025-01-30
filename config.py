d3ff5c7aa8077330bc7662a0e834508ed7695832
youfree4u <youfree4u@gmail.com>
2025-01-30 17:25:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "28625021"))
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "f1334d4192baa39e5fb10147559a354d")
 
 # Your Owner / Admin Id For Broadcast 
 ADMINS = int(os.environ.get("ADMINS", "6073523936"))
 
-# Your Mongodb Database Url
+mongodb+srv://nraajfcs:WD5jN3tdgJCNTd5n@cluster0.2x487.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = os.environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
